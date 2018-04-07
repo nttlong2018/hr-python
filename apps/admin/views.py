b5c1c3230e468dd91da5ec35f5491957a2a3532f
nttlong <zugeliang2000@gamil.com>
2018-04-08 00:50:47 +0700

    save

--- apps/admin/views.py
+++ apps/admin/views.py
@@ -4,6 +4,18 @@ import argo
 
 application=argo.get_application(__file__)
 
-@argo.template("index.html")
+@argo.template({
+    "file":"index.html",
+    "auth":"admin.auth.verify_user",
+    "login":"admin/login"
+})
 def index(request):
-    return  request.render({})
+    if request.get_auth()["user"]==None:
+        return redirect(request.get_app_url("login"))
+    if not request.get_auth()["user"].get("IsSysAdmin",False):
+        return redirect(request.get_app_url("login"))
+    else:
+         return  request.render({})
+@argo.template("login.html")
+def login(request):
+    return request.render({})
--- packages/argo/__init__.py
+++ packages/argo/__init__.py
@@ -1,6 +1,7 @@
 from . import config
 from . import utilities as utils
 from . import models
+from django.shortcuts import redirect
 import membership
 import threading
 import importlib
@@ -48,194 +49,238 @@ def get_application(file):
             file:ret_data
         })
     return __cache_app__[file]
-def template(file):
-    def template_decorator(fn):
+def template_uri(fn):
+    def layer(*args, **kwargs):
+        def repl(f):
+            return fn(f,*args, **kwargs)
+        return repl
+    return layer
+@template_uri
+def template(fn,_path):
+    fn.__dict__.update({"__params__": _path})
+    def exec_request(request):
+        file_path=fn.__dict__["__params__"]
+        auth_path = None
+        login_path = None
+        if type(file_path) is dict:
+            auth_path = file_path.get("auth", None)
+            login_path = file_path.get("login", None)
+            file_path = file_path.get("file", "")
+            if login_path == None and auth_path != None:
+                raise Exception("'auth' require 'login'. 'login' need to be set at '" + fn.__name__ + "'")
 
-        def exec_request(request):
-            global _language_engine_module
-            global _language_resource_cache
-            if _language_engine_module == None:
-                _language_engine_module = build_language_engine(config.get_default_language_engine())
+        global _language_engine_module
+        global _language_resource_cache
+        if _language_engine_module == None:
+            _language_engine_module = build_language_engine(config.get_default_language_engine())
 
-            language = "en"
-            if request.session.has_key("language"):
-                language=request.session["language"]
+        language = "en"
+        if request.session.has_key("language"):
+            language = request.session["language"]
 
-            app=get_application(fn.func_code.co_filename)
-            def set_auth(data):
-                global __session_cache__
+        app = get_application(fn.func_code.co_filename)
 
-                if not data.has_key("user"):
-                    raise Exception("Please set 'user' info {id:string,username:string}")
-                __session_cache__.update({
-                    request.session._get_session_key(): data
-                })
+        def set_auth(data):
+            global __session_cache__
+
+            if not data.has_key("user"):
+                raise Exception("Please set 'user' info {id:string,username:string}")
+            __session_cache__.update({
+                request.session._get_session_key(): data
+            })
+            request.session["authenticate"] = {
+                "user": data
+            }
+
+        def get_auth():
+            global __session_cache__
+            login_info = membership.validate_session(request.session._get_or_create_session_key())
+            if login_info == None:
                 request.session["authenticate"] = {
-                    "user": data
+                    "user": None
                 }
-            def get_auth():
-                global __session_cache__
-                login_info = membership.validate_session(request.session._get_or_create_session_key())
-                if login_info==None:
-                    request.session["authenticate"]={
-                        "user":None
+                __session_cache__.update({
+                    request.session._get_session_key(): {
+                        "user": None
                     }
-                    __session_cache__.update({
-                        request.session._get_session_key():{
-                            "user":None
-                        }
-                    })
-
-                else:
-                    request.session["authenticate"] = {
-                        "user": {
-                                "user_id": login_info.user.userId,
-                                "username": login_info.user.username,
-                                "email": login_info.user.email
-                            }
-                    }
-                    __session_cache__.update({
-                        request.session._get_session_key(): {
-                            "user": {
-                                "user_id": login_info.user.userId,
-                                "username": login_info.user.username,
-                                "email": login_info.user.email
-                            }
-                        }
-                    })
-
-                return __session_cache__[request.session._get_session_key()]
-            def render(model):
-                return utils.render({
-                    "app_name":app.name,
-                    "request":request,
-                    "language":"",
-                    "file":file,
-                    "model":model,
-                    "templates": app.template_dir,
-                    "file": file,
-                    "static": app.client_static,
-                    "application": app
-
-
                 })
-            def get_abs_url():
-                global __root_url__
-                if __root_url__ == None:
-                    if request.get_full_path() == "/":
-                        __root_url__ = request.build_absolute_uri()
-                    else:
-                        __root_url__ = request.build_absolute_uri().replace(
-                            request.get_full_path(), "")
-                    if __root_url__[__root_url__.__len__() - 1] == "/":
-                        __root_url__ = __root_url__[0:__root_url__.__len__() - 1]
-                return __root_url__
-            def get_language():
-                return language
 
-            def get_view_path():
-                ret = request.get_full_path()
-                if app.name == "default":
-                    if ret[0:1] == "/":
-                        ret = ret[1:ret.__len__()]
-                    return ret
+            else:
+                request.session["authenticate"] = {
+                    "user": {
+                        "user_id": login_info.user.userId,
+                        "username": login_info.user.username,
+                        "email": login_info.user.email
+                    }
+                }
+                __session_cache__.update({
+                    request.session._get_session_key(): {
+                        "user": {
+                            "user_id": login_info.user.userId,
+                            "username": login_info.user.username,
+                            "email": login_info.user.email
+                        }
+                    }
+                })
+
+            return __session_cache__[request.session._get_session_key()]
+
+        def render(model):
+            return utils.render({
+                "app_name": app.name,
+                "request": request,
+                "language": "",
+                "file": file_path,
+                "model": model,
+                "templates": app.template_dir,
+                "static": app.client_static,
+                "application": app
+
+            })
+
+        def get_abs_url():
+            global __root_url__
+            if __root_url__ == None:
+                if request.get_full_path() == "/":
+                    __root_url__ = request.build_absolute_uri()
                 else:
-                    if ret[0:1] == "/":
-                        ret = ret[1:ret.__len__()]
-                    ret = ret[app.host.__len__():ret.__len__()]
-                    if ret[0:1] == "/":
-                        ret = ret[1:ret.__len__()]
-                    return ret
+                    __root_url__ = request.build_absolute_uri().replace(
+                        request.get_full_path(), "")
+                if __root_url__[__root_url__.__len__() - 1] == "/":
+                    __root_url__ = __root_url__[0:__root_url__.__len__() - 1]
+            return __root_url__
 
-            def get_app_name():
-                return app.name
+        def get_language():
+            return language
 
-            def get_res(key, **caption):
-                if caption == None:
-                    caption = key
-                    key = key.lower()
-                lang_key = "language={};app={};view={};key={}".format(language, app.name,
-                                                                      get_view_path(), key)
-                ret_value = None
-                if not _language_resource_cache.has_key(lang_key):
-                    lock = threading.Lock()
-                    with lock:
+        def get_view_path():
+            ret = request.get_full_path()
+            if app.name == "default":
+                if ret[0:1] == "/":
+                    ret = ret[1:ret.__len__()]
+                return ret
+            else:
+                if ret[0:1] == "/":
+                    ret = ret[1:ret.__len__()]
+                ret = ret[app.host.__len__():ret.__len__()]
+                if ret[0:1] == "/":
+                    ret = ret[1:ret.__len__()]
+                return ret
+
+        def get_app_name():
+            return app.name
+
+        def get_res(key, **caption):
+            if caption == None:
+                caption = key
+                key = key.lower()
+            lang_key = "language={};app={};view={};key={}".format(language, app.name,
+                                                                  get_view_path(), key)
+            ret_value = None
+            if not _language_resource_cache.has_key(lang_key):
+                lock = threading.Lock()
+                with lock:
+                    ret_value = _language_engine_module.get_language_item(language, app.name,
+                                                                          get_view_path(), key, key)
+                    _language_resource_cache.update({
+                        lang_key: ret_value
+                    })
+
+            return _language_resource_cache[lang_key]
+
+        def get_app_res(key, **caption):
+            if caption == None:
+                caption = key
+                key = key.lower()
+            lang_key = "language={};app={};view={};key={}".format(language, app.name, "_", key)
+            ret_value = None
+            if not _language_resource_cache.has_key(lang_key):
+                lock = threading.Lock()
+                with lock:
+                    try:
                         ret_value = _language_engine_module.get_language_item(language, app.name,
-                                                                              get_view_path(), key, key)
+                                                                              "_", key, key)
                         _language_resource_cache.update({
                             lang_key: ret_value
                         })
+                    except Exception:
+                        pass
+            return _language_resource_cache[lang_key]
 
-                return _language_resource_cache[lang_key]
+        def get_global_res(key, **caption):
+            if caption == None:
+                caption = key
+                key = key.lower()
+            lang_key = "language={};app={};view={};key={}".format(language, "_", "_", key)
+            ret_value = None
+            if not _language_resource_cache.has_key(lang_key):
+                lock = threading.Lock()
+                with lock:
+                    try:
+                        ret_value = _language_engine_module.get_language_item(language, "_",
+                                                                              "_", key, key)
+                        _language_resource_cache.update({
+                            lang_key: ret_value
+                        })
+                    except Exception:
+                        pass
+            return _language_resource_cache[lang_key]
 
-            def get_app_res(key, **caption):
-                if caption == None:
-                    caption = key
-                    key = key.lower()
-                lang_key = "language={};app={};view={};key={}".format(language, app.name, "_", key)
-                ret_value = None
-                if not _language_resource_cache.has_key(lang_key):
-                    lock = threading.Lock()
-                    with lock:
-                        try:
-                            ret_value = _language_engine_module.get_language_item(language, app.name,
-                                                                                  "_", key, key)
-                            _language_resource_cache.update({
-                                lang_key: ret_value
-                            })
-                        except Exception:
-                            pass
-                return _language_resource_cache[lang_key]
+        def get_app_host():
+            if app.name == "default":
+                return ""
+            else:
+                return app.host
 
-            def get_global_res(key, **caption):
-                if caption == None:
-                    caption = key
-                    key = key.lower()
-                lang_key = "language={};app={};view={};key={}".format(language, "_", "_", key)
-                ret_value = None
-                if not _language_resource_cache.has_key(lang_key):
-                    lock = threading.Lock()
-                    with lock:
-                        try:
-                            ret_value = _language_engine_module.get_language_item(language, "_",
-                                                                                  "_", key, key)
-                            _language_resource_cache.update({
-                                lang_key: ret_value
-                            })
-                        except Exception:
-                            pass
-                return _language_resource_cache[lang_key]
+        def get_app_url(path):
+            if app.name == "default":
+                return get_abs_url() + "/" + path
+            else:
+                return get_abs_url() + "/" + get_app_host() + "/" + path
 
-            def get_app_host():
-                if app.name=="default":
-                    return  ""
+        def get_static(path):
+            return request.get_abs_url() + "/" + app.client_static + "/" + path
+
+        request.__dict__.update({"render": render})
+        request.__dict__.update({"set_auth": set_auth})
+        request.__dict__.update({"get_auth": get_auth})
+        request.__dict__.update({"get_app_host": get_app_host})
+        request.__dict__.update({"get_app_url": get_app_url})
+        request.__dict__.update({"get_abs_url": get_abs_url})
+        request.__dict__.update({"get_static": get_static})
+        request.__dict__.update({"get_language": get_language})
+        request.__dict__.update({"get_view_path": get_view_path})
+        request.__dict__.update({"get_app_name": get_app_name})
+        request.__dict__.update({"get_res": get_res})
+        request.__dict__.update({"get_app_res": get_app_res})
+        request.__dict__.update({"get_global_res": get_global_res})
+        if auth_path != None:
+            path_to_auth_fn = auth_path.split(".")[auth_path.split(".").__len__() - 1]
+            path_to_auth_mdl = auth_path[0: auth_path.__len__() - path_to_auth_fn.__len__() - 1]
+            import importlib
+            mdl=None
+            try:
+                mdl = importlib.import_module(path_to_auth_mdl)
+            except Exception as ex:
+                raise Exception("{0} was not found or error. Error message '{1}'\r\n"
+                                ". See '{2}' at '{3}' \r\n"
+                                "on file '{4}'".format(path_to_auth_mdl,ex.message,fn.__name__,fn.__module__,fn.func_code.co_filename))
+
+            try:
+                user=getattr(mdl, path_to_auth_fn)(request)
+                if user!=None:
+                    request.set_auth(user)
+                    fn(request)
                 else:
-                    return app.host
-            def get_app_url(path):
-                if app.name=="default":
-                    return get_abs_url()+"/"+path
-                else:
-                    return get_abs_url() + "/" + get_app_host() + "/" + path
+                    redirect(login_path)
+            except Exception as ex:
+                raise Exception("Error '{0}' at '{1}' in file '{2}'".format(ex.message,mdl.__name__,mdl.__file__))
 
-            def get_static(path):
-                return request.get_abs_url() + "/" + app.client_static + "/"+path
+        return fn(request)
 
-            request.__dict__.update({"render":render})
-            request.__dict__.update({"set_auth": set_auth})
-            request.__dict__.update({"get_auth": get_auth})
-            request.__dict__.update({"get_app_host": get_app_host})
-            request.__dict__.update({"get_app_url": get_app_url})
-            request.__dict__.update({"get_abs_url": get_abs_url})
-            request.__dict__.update({"get_static": get_static})
-            request.__dict__.update({"get_language":get_language})
-            request.__dict__.update({"get_view_path": get_view_path})
-            request.__dict__.update({"get_app_name":get_app_name})
-            request.__dict__.update({"get_res":get_res})
-            request.__dict__.update({"get_app_res": get_app_res})
-            request.__dict__.update({"get_global_res": get_global_res})
-            return fn(request)
-        return  exec_request
-    return template_decorator
+    return exec_request
+
+
 
 def build_language_engine(config):
     try:
@@ -249,6 +294,6 @@ def build_language_engine(config):
     except Exception as ex:
         logger.error(ex)
         raise Exception("Error in {0} with '{1}'".format(config.get("NAME",""),ex.message))
-def url_path(_url):
-    def dec_fn(fn):
+def auth(_url):
+    def authdecorator(fn):
         print (_url)
