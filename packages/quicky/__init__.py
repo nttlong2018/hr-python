028b05d9be4cd6abcb12e691540dbd793135f915
nttlong <zugeliang2000@gmail.com>
2018-05-11 18:10:37 +0700

    save

--- packages/quicky/__init__.py
+++ packages/quicky/__init__.py
@@ -8,8 +8,5 @@ import sql_db
 import os
 import layout_view
 import url
-from db import database as mongodb
-
-
 def get_static_server_path(file,path):
     return os.getcwd() + os.sep + os.path.dirname(file) + os.sep +path
